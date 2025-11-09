fc51953a01c42a173bd67c30a0e394161254cf30
Hamzawy <dark.knight_1993@hotmail.com>
2025-11-09 21:35:53 +0200

    Task 1 week 10

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -10,6 +10,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -26,5 +27,4 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
     def __str__(self):
-
         return f"{self.name} ({self.library.name})"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,37 @@
+# relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from django.views.generic import DetailView
+
+from .models import Book, Library
+
+def list_books(request):
+
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def list_books_plain(request):
+    books = Book.objects.select_related('author').all()
+    lines = [f"{b.title} by {b.author.name}" for b in books]
+    return HttpResponse('<br>'.join(lines), content_type='text/html')
+
+
+class LibraryDetailView(DetailView):
+
+
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        # جلب الكتب مختصراً (prefetch_related لتحسين الأداء مع ManyToMany)
+        ctx['books'] = self.object.books.select_related('author').all()
+        return ctx
+# relationship_app/views.py (إضافة)
 from django.shortcuts import render
 
-# Create your views here.
+def homepage(request):
+    return render(request, 'relationship_app/index.html')
