9c2a850fb617eab637e4ccf796d4d4dcc461e91d
Hamzawy <dark.knight_1993@hotmail.com>
2025-11-09 21:42:36 +0200

    Task 1 week 10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,16 @@
 # relationship_app/urls.py
 from django.urls import path
-from . import views
+
+from .views import list_books
+
+from .views import LibraryDetailView
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    path('', views.list_books, name='home'),
-    path('books/', views.list_books, name='list_books'),
-    path('books/plain/', views.list_books_plain, name='list_books_plain'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    # ✅ Function-based view: قائمة الكتب
+    path('books/', list_books, name='list_books'),
+
+    # ✅ Class-based view: تفاصيل مكتبة محددة
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
