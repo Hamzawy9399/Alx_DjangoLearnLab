770aef1dfa763706abf7ab5a824bb46789b08b71
Hamzawy <dark.knight_1993@hotmail.com>
2025-11-09 21:39:20 +0200

    Task 1 week 10

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,8 +2,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library
 
+from .models import Book, Library
 
 def list_books(request):
     """
