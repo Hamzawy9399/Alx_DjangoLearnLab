b7ff3c9aa5f1207775906e068cbbf8f68df49241
Hamzawy <dark.knight_1993@hotmail.com>
2025-12-08 00:20:16 +0200

    Task 0 week 14

---
+++ django_blog/django_blog/blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Post
+def index(request):
+    posts = Post.objects.select_related('author').order_by('-published_date')[:10]
+    return render(request, 'blog/index.html', {'posts': posts})
