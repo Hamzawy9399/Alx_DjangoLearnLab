dbc15d7176825c396a3293c983ba7f0cb06ced38
Hamzawy <dark.knight_1993@hotmail.com>
2025-12-14 22:03:16 +0200

    Task 0 week 14

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,29 @@
-from rest_framework import generics, permissions, status
+from rest_framework import viewsets, generics, permissions
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+Post.objects.all()
+Comment.objects.all()
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class FeedView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -10,4 +32,4 @@ class FeedView(generics.GenericAPIView):
         following_users = request.user.following.all()
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
